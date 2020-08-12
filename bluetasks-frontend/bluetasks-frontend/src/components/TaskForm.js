1ed040debbe350c52527fb2fbe285f3246db0a90
Lucas Galvao <lucas.engcomp@outlook.com>
2020-08-11 22:09:46 -0300

    Tratamento de load de dados

--- bluetasks-frontend/bluetasks-frontend/src/components/TaskForm.js
+++ bluetasks-frontend/bluetasks-frontend/src/components/TaskForm.js
@@ -1,10 +1,63 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class TaskForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      task: {
+        id: 0,
+        description: "",
+        whenToDo: "",
+      },
+    };
+
+    this.onSubmitHandler = this.onSubmitHandler.bind(this);
+    this.onInputChangeHandler = this.onInputChangeHandler.bind(this);
+  }
+
+  onSubmitHandler(event) {
+    event.preventDefault();
+    alert("entrou aqui");
+  }
+
+  onInputChangeHandler(event) {
+    console.log(event);
+  }
+
   render() {
     return (
       <div>
         <h1>Cadastro da tarefa</h1>
+        <form onSubmit={this.onSubmitHandler}>
+          <div className="form-group">
+            <label htmlFor="description">Descrição</label>
+            <input
+              type="text"
+              className="form-control"
+              name="description"
+              placeholder="Digite a descrição"
+              onChange={this.onInputChangeHandler}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="whenToDo">Data</label>
+            <input
+              type="date"
+              className="form-control"
+              name="whenToDo"
+              placeholder="Informe a data"
+              onChange={this.onInputChangeHandler}
+            />
+          </div>
+          <button type="submit" className="btn-primary">
+            Cadastrar
+          </button>
+          &nbsp;&nbsp;
+          <button type="button" className="btn-danger">
+            Cancelar
+          </button>
+        </form>
       </div>
     );
   }
