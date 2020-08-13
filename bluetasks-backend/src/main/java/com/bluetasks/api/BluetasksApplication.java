9866ed4452009fc8dbab05522d5ca28dc0b7e3fb
Lucas Galvao <lucas.engcomp@outlook.com>
2020-08-13 00:50:38 -0300

    Back end adicionado

---
+++ bluetasks-backend/src/main/java/com/bluetasks/api/BluetasksApplication.java
@@ -0,0 +1,13 @@
+package com.bluetasks.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BluetasksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BluetasksApplication.class, args);
+    }
+
+}
---
+++ bluetasks-backend/src/test/java/com/bluetasks/api/BluetasksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bluetasks.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BluetasksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
