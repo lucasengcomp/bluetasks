f7fa5cb98bd159baf3f9f32b0e10183d4400fc67
Lucas Galvao <lucas.engcomp@outlook.com>
2020-08-15 19:19:36 -0300

    Expondo ID da tarefa com o RepositoryRestConfigurer

--- bluetasks-backend/src/main/java/com/bluetasks/api/BluetasksApplication.java
+++ bluetasks-backend/src/main/java/com/bluetasks/api/BluetasksApplication.java
@@ -1,13 +1,20 @@
 package com.bluetasks.api;
 
+import com.bluetasks.api.domain.task.Task;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 
 @SpringBootApplication
-public class BluetasksApplication {
+public class BluetasksApplication implements RepositoryRestConfigurer {
 
     public static void main(String[] args) {
         SpringApplication.run(BluetasksApplication.class, args);
     }
 
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+        config.exposeIdsFor(Task.class);
+    }
 }
