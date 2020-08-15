be581f2e6caf9957bf339145efe2011701cb0a62
Lucas Galvao <lucas.engcomp@outlook.com>
2020-08-15 19:23:33 -0300

    Removendo exposição do respositório User

--- bluetasks-backend/src/main/java/com/bluetasks/api/domain/user/AppUserRepository.java
+++ bluetasks-backend/src/main/java/com/bluetasks/api/domain/user/AppUserRepository.java
@@ -2,9 +2,11 @@ package com.bluetasks.api.domain.user;
 
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@RepositoryRestResource(exported = false)
 public interface AppUserRepository extends CrudRepository<AppUser, Integer> {
 
     AppUser findByUsername(String username);
